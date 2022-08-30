aa65768cc154cc2690d5f91db33f909814a1c878
AButusov <boo2zz75384@mail.ru>
2022-08-30 16:14:07 +0300

    15

--- src/main/java/Java_Core_6/Main.java
+++ src/main/java/Java_Core_6/Main.java
@@ -1,8 +1,11 @@
 package Java_Core_6;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-            UserInterface userInterface = new UserInterface();
-            userInterface.runApplication();
+    public static void main(String[] args) throws SQLException, IOException {
+        UserInterface userInterface = new UserInterface();
+        userInterface.runApplication();
     }
 }
--- src/main/java/Java_Core_6/WeatherProvider.java
+++ src/main/java/Java_Core_6/WeatherProvider.java
@@ -1,9 +1,13 @@
 package Java_Core_6;
 
+import Java_Core_6.Entity.WeatherData;
 import Java_Core_6.Enums.Periods;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 public interface WeatherProvider {
-    public void getWeather(Periods period) throws IOException;
+    public void getWeather(Periods period) throws IOException, SQLException;
+    List<WeatherData> getAllFromDb() throws IOException, SQLException;
 }
